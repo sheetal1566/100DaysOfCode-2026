fbfb19e3e017917a7687130922ec6397dec532ca
vaishnavi757 <vaishnavisri504@gmail.com>
2026-06-21 21:56:42 +0530

    Update and rename Question1.cpp to Question1.c

--- Sunday_Debug/Sunday_debug_1/Vaishnavi_590017838/Question1.c
+++ Sunday_Debug/Sunday_debug_1/Vaishnavi_590017838/Question1.c
@@ -1,14 +1,13 @@
-#include <iostream>
-using namespace std;
-const int STADIUMS = 3;
-const int MAX_GOALS = 10;
-const int PLAYERS = 5;
+#include <stdio.h>
+#define STADIUMS 3
+#define MAX_GOALS 10
+#define PLAYERS 5
 int goals[STADIUMS][MAX_GOALS];
 int goalCount[STADIUMS] = {0};
 int playerGoals[PLAYERS] = {0};
 void addGoalToStadium2(int minute, int playerID) {
     if(goalCount[2] >= MAX_GOALS) {
-        cout << "Stadium 2 is full\n";
+        printf("Stadium 2 is full\n");
         return;
     }
     goals[2][goalCount[2]] = minute;
@@ -33,14 +32,12 @@ int topScorer() {
     }
     return winner;
 }
-
 void displayStadium(int stadium) {
     for(int i = 0; i < goalCount[stadium]; i++) {
-        cout << goals[stadium][i] << " ";
+        printf("%d ", goals[stadium][i]);
     }
-    cout << endl;
+    printf("\n");
 }
-
 int main() {
     goals[0][0] = 15;
     goals[0][1] = 42;
@@ -48,17 +45,17 @@ int main() {
     goals[1][0] = 8;
     goals[1][1] = 67;
     goalCount[1] = 2;
-
     addGoalToStadium2(23, 2);
     addGoalToStadium2(55, 2);
     addGoalToStadium2(80, 1);
-    cout << "Total Goals: " << totalGoals() << endl;
-    cout << "Top Scorer: Player " << topScorer() << endl;
-    cout << "Stadium 0 goals: ";
+
+    printf("Total Goals: %d\n", totalGoals());
+    printf("Top Scorer: Player %d\n", topScorer());
+    printf("Stadium 0 goals: ");
     displayStadium(0);
-    cout << "Stadium 1 goals: ";
+    printf("Stadium 1 goals: ");
     displayStadium(1);
-    cout << "Stadium 2 goals: ";
+    printf("Stadium 2 goals: ");
     displayStadium(2);
     return 0;
 }
